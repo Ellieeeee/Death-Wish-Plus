02f957bc1073ddf7d26b304c48b20c6dbd0ffc6b
Ellieeeee <32231454+Ellieeeee@users.noreply.github.com>
2017-10-03 19:42:14 -0500

    Update version

--- mods/Death Wish+/network/matchmaking/networkmatchmakingsteam.lua
+++ mods/Death Wish+/network/matchmaking/networkmatchmakingsteam.lua
@@ -1,4 +1,4 @@
 if DW.settings.dw_enemy_toggle_value then
 	--if you mess with this, you will crash others. don't come crying to me if that happens, this is here for a reason.
-	NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "deathwishplus 5.3.4"
+	NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "deathwishplus 5.3.5"
 end
